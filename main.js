da90189636c6d60dfd136ef2d585f8f9c9c172fd
Tim Guo <timguoqk@users.noreply.github.com>
2015-05-04 23:33:57 -0700

    add json data & lib

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var rawData;
+var candidateKeys = ["year", "name", "position", "endorsed", "won", "votePercentage"];
+
 $(function(){
-	
+	$.getJSON('./stats.json', function(data) {
+		rawData = data;
+	});
 });
+
